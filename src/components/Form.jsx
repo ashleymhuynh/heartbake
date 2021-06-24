94b07dbb85a04132e219f48369cd403a73bde7bb
ashleymhuynh <ashley.huynh23@gmail.com>
2021-06-24 12:07:02 -0700

    post new snack to Airtable and called useEffect upon submittal

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,40 @@
+import axios from "axios";
 import { useState } from "react";
+import { baseURL, config } from "../services";
+
 function Form(props) {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [rating, setRating] = useState(1);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newSnack = {
+      name,
+      description,
+      rating,
+    };
+    await axios.post(baseURL, { fields: newSnack }, config);
+    props.setToggleFetch((curr) => !curr);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="name">Name: </label>
       <input
         id="name"
         type="text"
         value={name}
         onChange={(e) => setName(e.target.value)}
+        autoComplete="off"
+        required
       />
       <label htmlFor="description">Description: </label>
       <textarea
         id="description"
         value={description}
         onChange={(e) => setDescription(e.target.value)}
+        required
       ></textarea>
       <label htmlFor="rating">Rating:</label>
       <input
@@ -26,6 +44,7 @@ function Form(props) {
         max={5}
         value={rating}
         onChange={(e) => setRating(e.target.valueAsNumber)}
+        required
       />
       <button type="submit">💔🍞</button>
     </form>
